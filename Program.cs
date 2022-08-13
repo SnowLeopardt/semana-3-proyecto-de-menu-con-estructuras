c27289c607db041dc8c1e0c52d22ad4e1ab0a15a
HENRY ANDERSON SANCHEZ CORTEZ <USSS030722@ugb.edu.sv>
2022-08-13 17:21:30 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace semana_3_proyecto_de_menu_con_estructuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto de menu sobre las estructuras");
+        }
+    }
+}
