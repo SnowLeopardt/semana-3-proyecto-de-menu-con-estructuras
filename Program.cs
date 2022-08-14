8335a03d68acffc2d16e716cf5869282b5360e54
HENRY ANDERSON SANCHEZ CORTEZ <USSS030722@ugb.edu.sv>
2022-08-13 18:05:15 -0600

    Finalizacion de la creacion del menu con sus estructuras

--- Program.cs
+++ Program.cs
@@ -2,11 +2,113 @@
 
 namespace semana_3_proyecto_de_menu_con_estructuras
 {
-    class Ejemplo_if
+    class Program
     {
-        static void Estructura_if()
+        static void Main(string[] args)
         {
-            System.Console.WriteLine("if externo");
+            System.Console.WriteLine("Proyecto de menu sobre las estructuras");
+            System.Console.WriteLine("Primer programa usando estructuras en modoconsola");
+            System.Console.WriteLine("Presione cualquier tecla para continuar");
+
+            System.Console.ReadKey();
+
+            int repetir;
+
+            do
+            {
+                Console.Clear();
+                menu_interno();
+
+                System.Console.WriteLine("Ingrese 1 para continuar el menu ");
+                System.Console.WriteLine("ingrese 2 para salir del menu");
+                repetir = Int32.Parse(Console.ReadLine());
+
+            } while (repetir == 1);
+
+                
+             
+        }
+
+        private static void menu_interno()
+        {
+            string opcion;
+            int opcionint;
+
+            System.Console.WriteLine("Menu Principal");
+            System.Console.WriteLine("1- Ejemplo If");
+            System.Console.WriteLine("2- Ejemplo Switch");
+            System.Console.WriteLine("3- Ejemplo For");
+            System.Console.WriteLine("4- Ejemplo While");
+            System.Console.WriteLine("5- Ejemplo Do While");
+
+            System.Console.WriteLine("Selecciona tu opcion");
+            opcion = Console.ReadLine();
+            opcionint = Int32.Parse(opcion);
+
+            switch (opcionint)
+            {
+                case 1:
+
+                    Programa_if();
+                    break;
+
+                case 2:
+
+                    Programa_switch();
+                    break;
+
+                case 3:
+
+                    programa_for();
+                    break;
+
+                case 4:
+
+                    progama_while();
+                    break;
+
+                case 5:
+
+                    programa_dowhile();
+                    break;
+
+                default:
+                    System.Console.WriteLine("opcion no valida");
+                    break;
+
+               
+              
+                        
+
+
+            }
+
+        }
+
+        private static void programa_dowhile()
+        {
+            System.Console.WriteLine("Aqui llamo el ejemplo Dowhile");
+
+        }
+
+        private static void progama_while()
+        {
+            System.Console.WriteLine("Aqui llamo el ejemplo While");
+        }
+
+        private static void programa_for()
+        {
+           System.Console.WriteLine("Aqui llamo el ejemplo For");
+        }
+
+        private static void Programa_switch()
+        {
+            System.Console.WriteLine("Aqui llamo al ejemplo Switch");
+        }
+
+        private static void Programa_if()
+        {
+            System.Console.WriteLine("Aqui llamo al ejemplo If");
         }
     }
 }
